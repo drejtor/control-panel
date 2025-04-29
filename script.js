f552100480bec9bee5421e6920789af38f4cc9c8
drejtor <123384868+drejtor@users.noreply.github.com>
2025-04-29 16:50:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+body {
+  font-family: Arial, sans-serif;
+  margin: 20px;
+}
+h1 {
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+nav ul {
+  list-style: none;
+  padding: 0;
+  margin-bottom: 30px;
+}
+nav ul li {
+  display: inline-block;
+  margin-right: 15px;
+}
+nav ul li a {
+  text-decoration: none;
+  color: blue;
+  font-weight: bold;
+}
+section {
+  margin-top: 40px;
+}
+iframe {
+  width: 100%;
+  height: 400px;
+  border: 1px solid #ccc;
+}
 
